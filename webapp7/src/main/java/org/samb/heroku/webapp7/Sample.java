7a13e99979edc0db5495dfd0a9cc06d0ac63a46d
devonforce <devonforce@192.168.1.43>
2017-10-08 21:37:48 +0530

    Initial checkin of webapp7

---
+++ webapp7/src/main/java/org/samb/heroku/webapp7/Sample.java
@@ -0,0 +1,10 @@
+package org.samb.heroku.webapp7;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hskgjh fjkgkhk gkjkjh gkdgkds");
+	}
+
+}
