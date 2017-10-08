a47cf216c308ded992b25794ffa176ca10c62b4d
devonforce <devonforce@192.168.1.43>
2017-10-08 21:59:10 +0530

    my second checkin

--- webapp7/src/main/java/org/samb/heroku/webapp7/Sample.java
+++ webapp7/src/main/java/org/samb/heroku/webapp7/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hskgjh fjkgkhk gkjkjh gkdgkds");
+		System.out.println("hskgjh fjkgkhk gkjkjh gkdgkds sfgdfhgdfhgdfdfgsnf,gmnf,gmn,mngh");
 	}
 
 }
